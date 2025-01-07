b85be694f1ac371449d2602ca5004c1cdebc7904
Rishabh Sharma <rish23.tech@gmail.com>
2025-01-07 17:16:08 +0530

    publish pipeline made

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,11 +1,34 @@
+import { config } from "dotenv";
+config();
 import { Server, Socket } from "socket.io";
+import Redis from "ioredis";
+
+// PUB SUB Architecture
+const pub = new Redis({
+  host: "redis-caching-1ea4e44b-rish23-8055.f.aivencloud.com",
+  port: 27325,
+  username: "default",
+  password: process.env.PUB_SUB_PWD,
+});
+
+const sub = new Redis({
+  host: "redis-caching-1ea4e44b-rish23-8055.f.aivencloud.com",
+  port: 27325,
+  username: "default",
+  password: process.env.PUB_SUB_PWD,
+});
 
 class SocketService {
   private _io: Server;
 
   constructor() {
     console.log("Initiating Socket Service...");
-    this._io = new Server();
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
   }
 
   public initListeners() {
@@ -16,7 +39,13 @@ class SocketService {
       console.log("New Socket Connected", socket.id);
 
       socket.on("event:message", async ({ message }: { message: string }) => {
-        console.log("New Message Received", message);
+        console.log("New Message Received:", message);
+
+        // Publish the message to Redis
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
+
+        // Optionally, broadcast the message to all connected sockets
+        io.emit("event:message:broadcast", { message });
       });
     });
   }
